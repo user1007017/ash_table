327d8c28a92d427cd3ca5f2794a38b3da840ab5f
mf <mf@netelite.ch>
2024-08-04 07:13:50 +0200

    sort icon

--- lib/ash_table/table_helpers.ex
+++ lib/ash_table/table_helpers.ex
@@ -25,7 +25,7 @@ defmodule AshTable.TableHelpers do
       phx-value-direction={sort_direction(@column, @sort)}
       class={sort_class(@column, @sort)}
     >
-      <%= @column.label %>
+      <%= @column.label %> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down-up ml-2"><path d="m3 16 4 4 4-4"></path><path d="M7 20V4"></path><path d="m21 8-4-4-4 4"></path><path d="M17 4v16"></path></svg>
     </button>
     """
   end
