567dde2037b6a60d133b2126790ff73b20aa79c2
mf <mf@netelite.ch>
2024-08-06 10:40:04 +0200

    icons instead of text

--- lib/ash_table/table_helpers.ex
+++ lib/ash_table/table_helpers.ex
@@ -37,12 +37,15 @@ defmodule AshTable.TableHelpers do
 
     <%!-- left --%>
 
-    <%= if @results.more? do %>
+    <%= if @results.offset < 1 do %>
     <button>
     <svg fill="#000000" width="24px" height="24px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
     <circle cx="16" cy="16" r="16"/>
     </svg>
-    <button>
+
+    <% else %>
+
+
     <% end %>
 
 
